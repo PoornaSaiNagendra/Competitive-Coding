5477be2b1f193bc47d0a45033c4b877819746f8a
PoornaSaiNagendra <42320447+PoornaSaiNagendra@users.noreply.github.com>
2024-01-28 09:48:30 +0530

    Time: 85 ms (88.60%) | Memory: 20.8 MB (97.82%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,13 +1,18 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        from collections import Counter
-        import heapq
-        freq = [(-1*value, key) for key, value in Counter(nums).items()]
-        heapq.heapify(freq)
-
+        freq_table = {}
+        for i in nums:
+            freq_table[i] = freq_table.get(i, 0) + 1
+        heap = []
+        for key, value in freq_table.items():
+            if len(heap) >= k: # If size is k then we dont want to increase the size further 
+                heappushpop(heap, (value, key))
+            elif len(heap) < k: # Size is not k then freely push values
+                heappush(heap, (value, key))
+		# After this operation the heap contains only k largest values of all the values in nums
         out = []
         for i in range(k):
-            out.append(heapq.heappop(freq)[-1])
+            out.append(heappop(heap)[1])
         return out
 
         
